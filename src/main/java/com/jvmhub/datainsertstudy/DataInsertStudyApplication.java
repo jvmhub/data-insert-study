c98848be60cbb60d9d22a922d99ac0b94eb9ca1e
hubek <niewiem15@op.pl>
2023-06-28 12:12:51 +0200

    spring.io init project

---
+++ src/main/java/com/jvmhub/datainsertstudy/DataInsertStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jvmhub.datainsertstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInsertStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataInsertStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvmhub/datainsertstudy/DataInsertStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmhub.datainsertstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataInsertStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
